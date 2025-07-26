1fe7dc1a5d2933b11805ea2b438eff01a4a58615
richardso21 <richardso2021@gmail.com>
2025-07-25 22:15:15 -0400

    project slug page impl

---
+++ src/routes/projects/[uid]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const { data } = $props();
+	const { project } = data;
+	const { title, image_url, uid } = project;
+
+	const href = `/projects/${uid}`;
+</script>
+
+<div class="relative w-full">
+	<h1 class="flip-title inline-block pb-10 text-6xl font-bold text-white sm:text-7xl md:text-8xl">
+		{title}
+	</h1>
+	<div data-flip-id={href} class="h-auto w-fit overflow-hidden rounded-md">
+		<img class="object-cover" src="/projects/{image_url}" alt="" />
+	</div>
+</div>
---
+++ src/routes/projects/[uid]/+page.ts
@@ -0,0 +1,18 @@
+import { projects } from '$lib/projects/projects';
+import type { ProjectMetaData } from '$lib/projects/projects.types';
+import { error } from '@sveltejs/kit';
+
+export function load({ params }): { project: ProjectMetaData } {
+	const { uid } = params;
+
+	const project = projects[uid];
+
+	if (!project) error(404, `Project [${uid}] not found`);
+
+	return {
+		project: {
+			...project,
+			uid
+		}
+	};
+}
