40b24374f33f81826647d1334086cac218867fe6
richardso21 <richardso2021@gmail.com>
2025-07-27 16:31:50 -0400

    fix experience item icon misaligned on mobile

--- src/lib/experience/ExperienceItem.svelte
+++ src/lib/experience/ExperienceItem.svelte
@@ -35,7 +35,9 @@
 </script>
 
 <li class="relative pb-10 pl-10 sm:pb-20 sm:pl-20">
-	<div class="experience-item-{key}-img absolute -left-10 h-12 w-12 scale-125 sm:h-20 sm:w-20">
+	<div
+		class="experience-item-{key}-img absolute -left-6 h-12 w-12 scale-125 sm:-left-10 sm:h-20 sm:w-20"
+	>
 		<img class="rounded-full" src="/experience/{experience.icon_url}" alt={experience.org} />
 		{#if experience.iconBackdrop}
 			<svg
