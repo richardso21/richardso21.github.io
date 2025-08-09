0672ff83279a70915ae132479879cce86f1934ab
richardso21 <richardso2021@gmail.com>
2025-08-08 20:12:13 -0400

    make projects list and item keyboard-friendly

--- src/lib/projects/ProjectItem.svelte
+++ src/lib/projects/ProjectItem.svelte
@@ -10,20 +10,34 @@
 
 	const getDateString = (date: Date) =>
 		date.toLocaleDateString('default', { month: 'short', year: 'numeric' });
+
+	const activated = (classes: string | string[]): string => {
+		if (typeof classes === 'string') {
+			classes = [classes];
+		}
+		return classes
+			.map((cls) => {
+				return `group-hover:${cls} group-focus-visible:${cls}`;
+			})
+			.join(' ');
+	};
 </script>
 
 <div data-flip-id={href} class="project-item relative h-[30vh] overflow-hidden rounded-md">
-	<a {href}>
-		<div class="group h-full w-full bg-gray-700 transition-all active:scale-95">
+	<a {href} class="group">
+		<div class="h-full w-full bg-gray-700 transition-all group-focus-visible:scale-95">
 			<img
-				class="block h-full w-full object-cover transition duration-200 group-hover:blur-[1px] group-hover:brightness-75"
+				class="block h-full w-full object-cover transition duration-200 {activated([
+					'blur-[1px]',
+					'brightness-75'
+				])}"
 				src="/projects/{image_url}"
 				alt=""
 			/>
 			<div
-				class="absolute left-0 z-10 w-full p-5 backdrop-blur-lg backdrop-brightness-75 transition-all duration-200 ease-out group-hover:bottom-0 {isMobile()
-					? 'bottom-0'
-					: '-bottom-40'}"
+				class="absolute left-0 z-10 w-full p-5 backdrop-blur-lg backdrop-brightness-75 transition-all duration-200 ease-out {activated(
+					'bottom-0'
+				)} {isMobile() ? 'bottom-0' : '-bottom-40'}"
 			>
 				<h2 class="pb-2 text-2xl font-bold text-white [text-shadow:0_0_5px_black] sm:text-3xl">
 					{title}
